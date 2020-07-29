7838de7d6f0b9aa3bee0284fc677c0052ba572a1
ankitrlps <rankit92@gmail.com>
2020-07-29 01:36:15 -0400

    Single Number leetcode program

---
+++ src/learning/algorithms/leetcode/easy/SingleNumber.java
@@ -0,0 +1,19 @@
+package learning.algorithms.leetcode.easy;
+
+import java.util.Arrays;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 3, 1, 5}; // {4, 4, 3, 2, 1, 3}
+        System.out.println(singleNumber(nums));
+    }
+    public static int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i+=2) {
+            if (nums[i] != nums[i-1]) {
+                return nums[i-1];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
