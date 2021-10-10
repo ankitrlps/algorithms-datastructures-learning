b343c210f257570e0414f2eed23f4bf702ca15a6
Ankit Renduchintala <rankit92@gmail.com>
2021-10-10 01:46:16 -0400

    adding length encoding solution

---
+++ src/learning/algorithms/RunLengthEncoding.java
@@ -0,0 +1,30 @@
+package learning.algorithms;
+
+public class RunLengthEncoding {
+
+    public static void main(String[] args) {
+        encodeString("aabbccde");
+    }
+
+    private static void encodeString(String input) {
+        if (input.length() == 0) {
+            return;
+        }
+        char[] chars = input.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        char prev = '\u0000'; // \u0000 denotes null for character datatype
+        for (char aChar : chars) {
+            if (prev != '\u0000' && aChar != prev) {
+                sb.append(prev).append(count);
+                count = 0;
+            }
+            count++;
+            prev = aChar;
+        }
+        if (count > 0) {
+            sb.append(prev).append(count);
+        }
+        System.out.println(sb);
+    }
+}
